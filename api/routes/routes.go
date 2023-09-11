2756775d2c2b6955e93647e499378808531e9386
alireza <alirezaarzehgar82@gmail.com>
2023-09-11 13:19:21 +0330

    api/routes: add some routes
    Back-end system used different path for each
    new versions of api.
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,8 +1,44 @@
 package routes
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func todo(c echo.Context) error {
+	return c.String(http.StatusOK, "version:"+c.Param("version"))
+}
+
+func groupedByVersion(g *echo.Group) {
+	g.POST("/register", todo)
+	g.POST("/login", todo)
+	g.GET("/users/:id", todo)
+	g.GET("/users", todo)
+	g.PUT("/users/:id", todo)
+	g.DELETE("/users/:id", todo)
+
+	g.POST("/flights", todo)
+	g.GET("/flights", todo)
+	g.GET("/flights/:id", todo)
+	g.PUT("/flights/:id", todo)
+	g.DELETE("/flights/:id", todo)
+
+	g.POST("/flights/:flight_id/booking", todo)
+	g.GET("/tickets", todo)
+	g.GET("/tickets/:id", todo)
+	g.GET("/tickets/:flight_id", todo)
+	g.PUT("/tickets/:id", todo)
+	g.DELETE("/tickets/:id", todo)
+
+	// We haven't plan for that
+	g.POST("/payments", todo)
+}
 
 func InitRoutes() *echo.Echo {
 	e := echo.New()
+
+	groupedByVersion(e.Group("/v:version"))
+
 	return e
 }
