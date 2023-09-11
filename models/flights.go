5da21224071c1b65ae00f1a3136b32ad5735a06d
alireza <alirezaarzehgar82@gmail.com>
2023-09-11 15:50:28 +0330

    models: add some models
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/flights.go
+++ models/flights.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"time"
+)
+
+type Flight struct {
+	ID            uint      `gorm:"primaryKey" json:"id"`
+	Origin        string    `gorm:"not null" json:"origin"`
+	Destination   string    `gorm:"not null" json:"destination"`
+	DepartureDate time.Time `gorm:"not null" json:"deoarture_date"`
+	ArrivalDate   time.Time `gorm:"not null" json:"arrival_date"`
+	Price         float64   `gorm:"not null" json:"price"`
+}
--- models/tickets.go
+++ models/tickets.go
@@ -1,1 +1,13 @@
 package models
+
+import "time"
+
+type Ticket struct {
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	UserID      uint      `gorm:"not null" json:"user_id"`
+	FlightID    uint      `gorm:"not null" json:"flight_id"`
+	Flight      Flight    `json:"flight,omitempty"`
+	Users       []User    `gorm:"many2many:user_tickets" json:"passengers"`
+	TotalPrice  float64   `gorm:"-" json:"total_price"`
+	BookingDate time.Time `gorm:"not null" json:"booking_date"`
+}
