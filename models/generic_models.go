3a10faca85918998884f7cd708e5b48b4dce2de5
alireza <alirezaarzehgar82@gmail.com>
2023-09-11 17:24:49 +0330

    models: add generic `Create` function
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/generic_models.go
+++ models/generic_models.go
@@ -1,1 +1,30 @@
 package models
+
+import (
+	"errors"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func ErrGormToHttp(r *gorm.DB) *echo.HTTPError {
+	err := r.Error
+	switch {
+	case errors.Is(err, gorm.ErrRecordNotFound):
+	case errors.Is(err, gorm.ErrForeignKeyViolated):
+		return echo.ErrNotFound
+	case errors.Is(err, gorm.ErrDuplicatedKey):
+		return echo.ErrConflict
+	}
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+	if r.RowsAffected == 0 {
+		return echo.ErrNotFound
+	}
+	return nil
+}
+
+func Create[T any](model *T) error {
+	return ErrGormToHttp(db.Create(&model))
+}
