8edd766587c573b20d3115c9438f344925828be0
alireza <alirezaarzehgar82@gmail.com>
2023-09-11 16:38:28 +0330

    models: replace new Passenger model with User on Ticket
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/tickets.go
+++ models/tickets.go
@@ -2,12 +2,20 @@ package models
 
 import "time"
 
+type Passenger struct {
+	ID        uint   `gorm:"primaryKey" json:"id"`
+	TicketID  uint   `json:"ticket_id"`
+	FirstName string `gorm:"not null" json:"first_name"`
+	LastName  string `gorm:"not null" json:"last_name"`
+	Email     string `gorm:"not null; unique" json:"email"`
+}
+
 type Ticket struct {
-	ID          uint      `gorm:"primaryKey" json:"id"`
-	UserID      uint      `gorm:"not null" json:"user_id"`
-	FlightID    uint      `gorm:"not null" json:"flight_id"`
-	Flight      Flight    `json:"flight,omitempty"`
-	Users       []User    `gorm:"many2many:user_tickets" json:"passengers"`
-	TotalPrice  float64   `gorm:"-" json:"total_price"`
-	BookingDate time.Time `gorm:"not null" json:"booking_date"`
+	ID          uint        `gorm:"primaryKey" json:"id"`
+	UserID      uint        `gorm:"not null" json:"user_id"`
+	FlightID    uint        `gorm:"not null" json:"flight_id"`
+	Flight      Flight      `json:"flight,omitempty"`
+	Passengers  []Passenger `json:"passengers"`
+	TotalPrice  float64     `gorm:"-" json:"total_price"`
+	BookingDate time.Time   `gorm:"not null" json:"booking_date"`
 }
