9c1e63b384b94cc58997ca0157288263a8320c22
alireza <alirezaarzehgar82@gmail.com>
2023-09-11 17:23:47 +0330

    api/handlers: add prototypes
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- api/handlers/users.go
+++ api/handlers/users.go
@@ -1,1 +1,27 @@
 package handlers
+
+import "github.com/labstack/echo/v4"
+
+func Register(c echo.Context) error {
+	return nil
+}
+
+func Login(c echo.Context) error {
+	return nil
+}
+
+func FetchUser(c echo.Context) error {
+	return nil
+}
+
+func FetchUsers(c echo.Context) error {
+	return nil
+}
+
+func EditUser(c echo.Context) error {
+	return nil
+}
+
+func DeleteUser(c echo.Context) error {
+	return nil
+}
