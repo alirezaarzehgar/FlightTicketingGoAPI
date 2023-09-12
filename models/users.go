7bacf06b07b45b896a7ed3b7dafb7ffdc300ed7f
alireza <alirezaarzehgar82@gmail.com>
2023-09-12 19:04:26 +0330

    models: fix unique tag problem and omitempty on tickets
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/users.go
+++ models/users.go
@@ -8,8 +8,8 @@ const (
 
 type User struct {
 	ID       uint     `gorm:"primaryKey" json:"id"`
-	Email    string   `gorm:"not null; unique" json:"email,omitempty"`
+	Email    string   `gorm:"not null;unique" json:"email,omitempty"`
 	Password string   `gorm:"not null" json:"password,omitempty"`
 	Role     string   `gorm:"not null" json:"role,omitempty"`
-	Tickets  []Ticket `gorm:"many2many:user_tickets"`
+	Tickets  []Ticket `gorm:"many2many:user_tickets" json:"tickets,omitempty"`
 }
