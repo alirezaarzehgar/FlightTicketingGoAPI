178887b6a3eef675ea763ecfd0bc2409dff9dd2b
alireza <alirezaarzehgar82@gmail.com>
2023-09-13 21:14:42 +0330

    *: Use hasone relationship for Flight airline origin and dests
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/flights.go
+++ models/flights.go
@@ -6,8 +6,10 @@ import (
 
 type Flight struct {
 	ID            uint      `gorm:"primaryKey" json:"id"`
-	Origin        string    `gorm:"not null" json:"origin"`
-	Destination   string    `gorm:"not null" json:"destination"`
+	OriginID      uint      `gorm:"not null" json:"origin"`
+	Origin        Airline   `gorm:"foreignKey:OriginID" json:"origin_id"`
+	DestinationID uint      `gorm:"not null" json:"destination_id"`
+	Destination   Airline   `gorm:"foreignKey:DestinationID" json:"destination"`
 	DepartureDate time.Time `gorm:"not null" json:"departure_date"`
 	ArrivalDate   time.Time `gorm:"not null" json:"arrival_date"`
 	Price         float64   `gorm:"not null" json:"price"`
